6e263f3c1406f5564c0c8f2671d23f9f0d2f30cd
seita <seitaroumiura@gmail.com>
2019-11-13 17:25:01 +0900

    first commit

---
+++ Master/osero.py
@@ -0,0 +1,22 @@
+class  Board():
+    
+    def __init__(self):
+        pass
+
+    def GetBoard(self):
+        pass
+
+    def PutPiece(self, x, y):
+        pass
+
+    def PrintBoard(self):
+        pass
+
+    def SearchBoard(self):
+        pass
+
+    def CheckBoard(self, x, y):
+        pass
+
+    def CountPiece(self, color):
+        pass
