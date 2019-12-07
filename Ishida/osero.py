f87bbad726a4d845d14177628c566a2e3ff76018
Hikarin <Ishida.Hikaru@outlook.jp>
2019-12-07 15:14:15 +0900

    MiuraのOsero.pyをIshidaのフォルダ内にコピー

--- Ishida/osero.py
+++ Ishida/osero.py
@@ -1,7 +1,11 @@
+# cSpell: ignore Miura Ishida numpy
+import copy
 import numpy as np
+import Test
+
 
 class Board():
-    
+
     # メンバ変数とフィールドの初期化
     def __init__(self):
         # メンバ変数の初期化
@@ -23,18 +27,64 @@ class Board():
         self.__init__()
 
     # Miura
+    # 現在のボードの配列を返す
     def GetBoard(self):
-        pass
+        return self.field
 
     # Miura
+    # 指定された場所に駒を置く
+    # 置けなかったらエラーを出す
     def PutPiece(self, x, y):
-        pass
+        # 駒を置けるかを確認
+        if not [x, y] in self.SearchBoard(self.turn):
+            print(self.SearchBoard(self.turn))
+            raise RuntimeError("指定された場所に駒を置くことができません。")
+        # self.turn = self.WHITE
+        self.field[x][y] = self.turn
+
+        # 裏返しにする方向の決定
+        directions = [
+            [1, 0], [-1, 0], [0, 1], [0, -1],
+            [1, 1], [-1, 1], [-1, 1], [-1, -1]
+        ]
+        for d in directions:
+            pos_x = x
+            pos_y = y
+            # *pythonには浅いコピーと深いコピーがある
+            # *浅いコピーでは、コピー元のデータが書き変えられると
+            # *コピー先の内容も変わってしまう。
+            # *https://techblog.recochoku.jp/515
+            copyBoard = copy.deepcopy(self.field)
+            while(True):
+                print("ok")
+                pos_x += d[0]
+                pos_y += d[1]
+
+                # 端まで行ったらループを終了
+                if not (0 < pos_x < 8 or 0 < pos_y < 8):
+                    print(1)
+                    self.field = copyBoard
+                    break
+                if self.field[pos_x][pos_y] != self.turn and self.field[pos_x][pos_y] != self.EMPTY:  # 相手の駒があった場合
+                    self.field[pos_x][pos_y] = self.turn
+                if self.field[pos_x][pos_y] == self.EMPTY:
+                    self.field = copyBoard
+                    break
+                if self.field[pos_x][pos_y] == self.turn:
+                    break
 
     # Miura
+    # CUIで駒を表示します。
     def PrintBoard(self):
-        pass
+        CHAR_PEACE = [" ", "o", "*"]
+        for i in self.field:
+            print(end="|")
+            for j in i:
+                print(CHAR_PEACE[int(j)], end="|")
+            print("\n------------------")
 
     # Ishida
+    # return: [y, x]
     def SearchBoard(self, color):
         c_list = np.empty((0, 2), int)
         result = np.empty((0, 2), int)
@@ -69,6 +119,70 @@ class Board():
     def CountPiece(self, color):
         return np.count_nonzero(self.field == color)
 
+
 if __name__ == "__main__":
-    Test = Board()
-    print(Test.SearchBoard(Test.WHITE))
+    b = Board()
+    B = b.BLACK
+    W = b.WHITE
+    b.field = [
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, B, W, 0, 0, 0],
+        [0, 0, 0, B, W, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0],
+    ]
+    print("BLACK")
+    res = b.SearchBoard(b.BLACK)
+    b.PrintBoard()
+    print(res)
+    
+    print("\nWHITE")
+    res = b.SearchBoard(b.WHITE)
+    b.PrintBoard()
+    print(res)
+
+    # ### 疑問点
+    # 置ける場所の内、左横が見落とされている？
+    # ### 実行結果
+    #   BLACK
+    #   | | | | | | | | |
+    #   ------------------
+    #   | | | | | | | | |
+    #   ------------------
+    #   | | | | | | | | |
+    #   ------------------
+    #   | | | |*|o| | | |
+    #   ------------------
+    #   | | | |*|o| | | |
+    #   ------------------
+    #   | | | | | | | | |
+    #   ------------------
+    #   | | | | | | | | |
+    #   ------------------
+    #   | | | | | | | | |
+    #   ------------------
+    #   [[2 2]
+    #    [3 2]]
+
+    #   WHITE
+    #   | | | | | | | | |
+    #   ------------------
+    #   | | | | | | | | |
+    #   ------------------
+    #   | | | | | | | | |
+    #   ------------------
+    #   | | | |*|o| | | |
+    #   ------------------
+    #   | | | |*|o| | | |
+    #   ------------------
+    #   | | | | | | | | |
+    #   ------------------
+    #   | | | | | | | | |
+    #   ------------------
+    #   | | | | | | | | |
+    #   ------------------
+    #   [[2 5]
+    #    [3 5]]
